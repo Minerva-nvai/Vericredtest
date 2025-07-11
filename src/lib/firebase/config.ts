6621372ea16ccd2a47791c8bf8d131749dbfc5aa
Krish Goenka <goenkakrish02@gmail.com>
2025-07-11 16:34:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -2,15 +2,16 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// TODO: Add your own Firebase configuration from your project settings
+// Your web app's Firebase configuration from your project settings
 // https://firebase.google.com/docs/web/setup#available-libraries
+// It's best practice to use environment variables for this
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
